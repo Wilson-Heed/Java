0c8dab063e420c45253ccd0f24d9c1fad07d62a3
Wilson-Heed <lawalwilson1@gmail.com>
2023-10-04 01:21:47 -0700

    Assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+let fName = "Lawal"
+let lname =  " Rasheed"
+let fullName = fName +  lname
+console.log(fullName)
+let age = "25"
+console.log(age)
+let x = "ismale"
+let y = "ismale"
+console.log(x == y)
+const courses = ["Mathematics", "Physics", "English", "Biology"];
+console.log(courses)
+const northCentral =[ 'Benue', 'Kogi', 'Kwara', 'Nasarawa', 'Niger', 'plateau', 'Abuja']
+const northeast =[ 'Adamawa', 'Bauchi', 'Borno', 'Gombe', 'Taraba',  'Yobe']
+const northwest =[ 'Jigawa', 'Kaduna', 'Kano', 'Katsina', 'Kebbi', 'Sokoto', 'Zamfara']
+const output3 =document.getElementById('output3').innerHTML=(northwest)
+const output2 =document.getElementById('output2').innerHTML=(northeast)
+const output =document.getElementById('output').innerHTML=(northCentral)
+const southeast =[ 'Abia', 'Anambra', 'Ebonyi', 'Enugu', 'Imo']
+const southsouth =[ 'Akwa Ibom', 'Bayelsa', 'Cross River', 'Delta', 'Edo', 'Rivers']
+const southwest =[ 'Ekiti', 'Lagos', 'Ogun', 'Ondo', 'Osun', 'Oyo']
+const output6 =document.getElementById('output6').innerHTML=(southwest)
+const output5 =document.getElementById('output5').innerHTML=(southsouth)
+const output4 =document.getElementById('output4').innerHTML=(southeast)
